81d33bbbe0014e6f2a7ff7a24bc01adb3eeae3b1
Jay-R-H <jayhiremathr@gmail.com>
2021-10-02 22:21:32 +0530

    Added:Daily employee wage computation

--- src/com/programming/EmployeeWageBuilder.java
+++ src/com/programming/EmployeeWageBuilder.java
@@ -4,15 +4,21 @@ import java.util.Random;
 
 public class EmployeeWageBuilder {
     public static final int isPresent = 1;
+    public static final int wagePerHour =20;
+    public static final int hoursPerDay =8;
 
     public static void main(String[] args) {
         System.out.println("Welcome to employee computation program");
+        int wage=0;
         int employeeCheck = (int) Math.floor((Math.random() * 10) % 2);
 
         if (employeeCheck == isPresent) {
             System.out.println("Employee is present");
-        } else {
+            wage=wagePerHour*hoursPerDay;
+        }
+        else {
             System.out.println("Employee is absent");
         }
+        System.out.println("Employee has earned " + wage);
     }
 }
