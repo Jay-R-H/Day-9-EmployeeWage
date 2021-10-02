9c15f92814541f9a4dc9e4060217da1bd8577373
Jay-R-H <jayhiremathr@gmail.com>
2021-10-02 22:26:45 +0530

    Added:Part time employee attendance

--- src/com/programming/EmployeeWageBuilder.java
+++ src/com/programming/EmployeeWageBuilder.java
@@ -3,22 +3,26 @@ package com.programming;
 import java.util.Random;
 
 public class EmployeeWageBuilder {
-    public static final int isPresent = 1;
-    public static final int wagePerHour =20;
-    public static final int hoursPerDay =8;
+    public static final int isFullTime = 1;
+    public static final int isPartTime = 2;
+    public static final int wagePerHour = 20;
+
 
     public static void main(String[] args) {
         System.out.println("Welcome to employee computation program");
-        int wage=0;
-        int employeeCheck = (int) Math.floor((Math.random() * 10) % 2);
+        int wage = 0;
+        int workingHrs = 0;
+        int employeeCheck = (int) Math.floor((Math.random() * 10) % 3);
 
-        if (employeeCheck == isPresent) {
-            System.out.println("Employee is present");
-            wage=wagePerHour*hoursPerDay;
-        }
-        else {
+        if (employeeCheck == isFullTime) {
+            workingHrs = 8;
+        } else if (employeeCheck == isPartTime) {
+            workingHrs = 4;
+        } else {
+            workingHrs = 0;
             System.out.println("Employee is absent");
         }
+        wage = wagePerHour * workingHrs;
         System.out.println("Employee has earned " + wage);
     }
 }
