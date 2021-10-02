afe6a080c6fb629d275af7be13b133fe857ab57b
Jay-R-H <jayhiremathr@gmail.com>
2021-10-02 22:31:16 +0530

    Added:Solving by using switch case

--- src/com/programming/EmployeeWageBuilder.java
+++ src/com/programming/EmployeeWageBuilder.java
@@ -14,15 +14,18 @@ public class EmployeeWageBuilder {
         int workingHrs = 0;
         int employeeCheck = (int) Math.floor((Math.random() * 10) % 3);
 
-        if (employeeCheck == isFullTime) {
-            workingHrs = 8;
-        } else if (employeeCheck == isPartTime) {
-            workingHrs = 4;
-        } else {
-            workingHrs = 0;
-            System.out.println("Employee is absent");
+        switch (employeeCheck) {
+            case isFullTime:
+                workingHrs = 8;
+                break;
+            case isPartTime:
+                workingHrs = 4;
+                break;
+            default:
+                workingHrs = 0;
+                System.out.println("Employee is absent");
         }
         wage = wagePerHour * workingHrs;
-        System.out.println("Employee has earned " + wage);
+        System.out.println("Employee has earned " + wage );
     }
 }
