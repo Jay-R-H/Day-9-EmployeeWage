4b73c81697bcd281ec6909bb16b19d34ea3869c4
Jay-R-H <jayhiremathr@gmail.com>
2021-10-02 22:39:24 +0530

    Added:Employee monthly wage calculation

--- src/com/programming/EmployeeWageBuilder.java
+++ src/com/programming/EmployeeWageBuilder.java
@@ -6,26 +6,35 @@ public class EmployeeWageBuilder {
     public static final int isFullTime = 1;
     public static final int isPartTime = 2;
     public static final int wagePerHour = 20;
+    public static final int totalWorkingDays = 20;
 
 
     public static void main(String[] args) {
         System.out.println("Welcome to employee computation program");
         int wage = 0;
         int workingHrs = 0;
-        int employeeCheck = (int) Math.floor((Math.random() * 10) % 3);
+        int workingDays = 0;
+        int totalWage = 0;
 
-        switch (employeeCheck) {
-            case isFullTime:
-                workingHrs = 8;
-                break;
-            case isPartTime:
-                workingHrs = 4;
-                break;
-            default:
-                workingHrs = 0;
-                System.out.println("Employee is absent");
+        while (totalWorkingDays > workingDays) {
+            int employeeCheck = (int) Math.floor((Math.random() * 10) % 3);
+            workingDays++;
+            switch (employeeCheck) {
+                case isFullTime:
+                    workingHrs = 8;
+                    System.out.println("Employee has worked for " + workingHrs);
+                    break;
+                case isPartTime:
+                    workingHrs = 4;
+                    System.out.println("Employee has worked for " + workingHrs);
+                    break;
+                default:
+                    workingHrs = 0;
+                    System.out.println("Employee is absent");
+            }
+            wage = wagePerHour * workingHrs;
+            totalWage += wage;
         }
-        wage = wagePerHour * workingHrs;
-        System.out.println("Employee has earned " + wage );
+        System.out.println("Employee has earned " + totalWage);
     }
 }
